c6ed6371c8f6278468dcf18870a232d071b7ef8a
Jonathon Choo <jonno.choo@gmail.com>
2014-11-13 12:03:15 +1100

    Initial version

---
+++ choco.ps1
@@ -0,0 +1,19 @@
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install 7zip
+choco install dropbox
+choco install evernote
+choco install fiddler
+choco install filezilla 
+choco install foxitreader
+choco install googlechrome
+choco install keepass-classic
+choco install launchy
+choco install nodejs 
+choco install paint.net 
+choco install poshgit 
+choco install skitch
+choco install spotify
+choco install sublime 
+choco install TortoiseGit
+choco install virtualbox 
