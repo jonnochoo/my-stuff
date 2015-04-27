85f5eeddd7c7aaa59b22b09a93ab19cd55bb52d2
Jonathon Choo <jonno.choo@gmail.com>
2015-04-27 12:18:11 +1000

    Added -y to choco commands
    New version of chocolatey prompts for user input so -y avoids this

--- setup/windows/windows-setup.ps1
+++ setup/windows/windows-setup.ps1
@@ -1,25 +1,25 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install 7zip
-choco install cpu-z
-choco install dropbox
-choco install evernote
-choco install fiddler
-choco install filezilla 
-choco install sumatrapdf
-choco install googlechrome
-choco install ilspy
-choco install keepass-classic
-choco install launchy
-choco install nodejs 
-choco install paint.net 
-choco install poshgit 
-choco install skitch
-choco install spotify
-choco install sublime 
-choco install TortoiseGit
-choco install virtualbox 
-choco install windirstat
-choco install hipchat
+choco -y install 7zip
+choco -y install cpu-z
+choco -y install dropbox
+choco -y install evernote
+choco -y install fiddler
+choco -y install filezilla 
+choco -y install sumatrapdf
+choco -y install googlechrome
+choco -y install ilspy
+choco -y install keepass-classic
+choco -y install launchy
+choco -y install nodejs 
+choco -y install paint.net 
+choco -y install poshgit 
+choco -y install skitch
+choco -y install spotify
+choco -y install sublime 
+choco -y install TortoiseGit
+choco -y install virtualbox 
+choco -y install windirstat
+choco -y install hipchat
 
 choco install vagrant
 npm install azure-cli -g
